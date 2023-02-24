c89f21bf40beb734542578a888410ffdb8eaca1f
Your Name <abesulke@gmail.com>
2023-02-23 20:25:09 -0800

    claritech first page

---
+++ js/Day-1/day-1.2.js
@@ -0,0 +1,9 @@
+// Database
+
+var a = 12;
+var b = `codefirst`;
+var c = true;
+var d = null;
+var c = undefined;
+
+console.log(typeof(d));
---
+++ js/Day-1/day-1.js
@@ -0,0 +1,29 @@
+var number= 20;
+alert(number);
+
+// var number= expression(value)
+
+var a= 10;
+var b= 20;
+var result= a + b;
+console.log (result)
+
+// variable use
+
+var num12er3= 40;
+var _123;
+var x;
+// variable declaration 
+var x= result;
+
+var d = "test";
+var e = 123;
+var f = 12.5;
+var g = false;
+ 
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+
+
